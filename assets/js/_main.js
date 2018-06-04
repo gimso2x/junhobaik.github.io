ff00872777b01101d8cc4ce3eaaa59ef2be29a46
Gimsoso <gimso2x@gmail.com>
2018-06-04 17:44:00 +0900

    Update _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -3,6 +3,23 @@
    ========================================================================== */
 
 $(document).ready(function() {
+   //sidebar close script 18-06-04
+   $('.nav__sub-title').click(function() {
+      $item = $(this);
+      $target = $(this).next('ul');
+      
+      if($item.hasClass('active')) {
+         $item.removeClass('active');
+         $target.stop().slideUp('fast');
+      } else {
+         $item.addClass('active');
+         $target.stop().slideDown('fast');
+      }
+      
+      $item.siblings().removeClass('active');
+      $item.siblings().next('ul').stop().slideUp('fast');
+   }
+   
   // Sticky footer
   var bumpIt = function() {
       $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
